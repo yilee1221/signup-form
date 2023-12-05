0cf1faadd26039abfc9d6e9e6e67f0ead09c812e
Erin Lee <123915538+yilee1221@users.noreply.github.com>
2023-12-05 10:53:59 +0900

    added password checker

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const submitButton = document.querySelector('button');
 const password = document.querySelector('#password');
+const requirements = document.querySelector('.requirements');
 const confirmpw = document.querySelector('#confirm');
 const error = document.querySelector('.error');
 
-function checkPassword() {
+function confirmPassword() {
     if (confirmpw.value === password.value) {
         error.textContent = "";
     }
@@ -12,6 +13,16 @@ function checkPassword() {
     }
 }
 
-password.addEventListener('input', checkPassword);
-confirmpw.addEventListener('input', checkPassword);   
+password.addEventListener('input', confirmPassword);
+confirmpw.addEventListener('input', confirmPassword);   
 
+function checkPassword() {
+    if (password.checkValidity()) {
+        requirements.textContent = "";
+    }
+    else {
+        requirements.textContent = "Minimum 8 characters, one uppercase and lowercase letter, one number, one special character";
+    }
+}
+
+password.addEventListener('input', checkPassword);
